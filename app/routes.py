a134e2c2bd8e292c7c641990382451a297704fc8
xuezhishan <809484171@qq.com>
2018-10-11 19:51:01 +0800

    learn to Chpt5

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
 from app import app
-from flask import render_template, flash, redirect,url_for
+from flask import render_template, flash, redirect,url_for,request
 from app.forms import LoginForm
+from app.models import User
+from flask_login import current_user,login_user,logout_user,login_required
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
-@app.route('/home')
+@login_required
 def index():
 	#return "Hello World!"
 	user = {'username':'XUEZHIHSAN'}
@@ -18,7 +21,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-	return render_template('index.html', title='Home', user=user, posts = posts)
+	return render_template('index.html', title='Home', posts = posts)
 	#return render_template('index.html', user=user)
 
 '''
@@ -30,9 +33,28 @@ def login():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+
+	form = LoginForm()
+
+	if form.validate_on_submit():
+		user=User.query.filter_by(username=form.username.data).first()
+		print(user)
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password')
+			return redirect(url_for('login'))
+		login_user(user,remember=form.remember_me.data)
+		next_page=request.args.get('next')
+		if not next_page or url_parse(next_page).netloc!='':
+			next_page=url_for('index')
+		return redirect(next_page)
          #flash()函数是向用户显示消息的有效途径
-        flash('Login requested for user={},remeber_me={}'.format(form.username.data,form.remember_me.data))
-        return redirect(url_for('index'))#直接在模板和源文件中硬编码链接存在隐患，如果有一天你决定重新组织链接，那么你将不得不在整个应用中搜索并替换这些链接
-    return render_template('login.html',title='Sign In',form=form)
+        #flash('Login requested for user={},remeber_me={}'.format(form.username.data,form.remember_me.data))
+        #return redirect(url_for('index'))#直接在模板和源文件中硬编码链接存在隐患，如果有一天你决定重新组织链接，那么你将不得不在整个应用中搜索并替换这些链接
+	return render_template('login.html',title='Sign In',form=form)
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
